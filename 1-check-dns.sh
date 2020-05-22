2ae5e3c78e256b33944d983d70ae1218acb449af
root <root@dell-r640-10.gsslab.pnq2.redhat.com>
2020-05-22 20:55:00 +0530

    modify shell

--- 1-check-dns.sh
+++ 1-check-dns.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+set -x
 echo "1.2.3.4 test.local" >> /etc/hosts
 systemctl restart libvirtd
+
+sleep 2
 dig test.local @${HOST_IP}
 dig -x 1.2.3.4 @${HOST_IP}
+
 echo "srv-host=test.local,yayyy.local,2380,0,10" > ${DNS_DIR}/temp-test.conf
+
 systemctl reload NetworkManager
+
 dig srv test.local
 dig srv test.local @${HOST_IP}
 rm -rf ${DNS_DIR}/temp-test.conf
 sed -i '/test\.local/d' /etc/hosts
+
+
+set +x
